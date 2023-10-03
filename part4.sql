68e3cb5a4282b309af2e7bb82a36efdcca536d2e
Craft Becca <craftbec@oa-e4.msk.21-school.ru>
2023-10-03 16:21:07 +0300

    part4.4

--- part4.sql
+++ part4.sql
@@ -1,52 +1,53 @@
 DROP FUNCTION IF EXISTS GetDates(integer);
 CREATE FUNCTION GetDates(key integer)
-    RETURNS SETOF date
-	AS $$
-    BEGIN
-        IF (key = 1) THEN
-        RETURN QUERY
-        SELECT MAX(date(transaction_datetime))
-		FROM transactions;
-        ELSEIF (key = 2) THEN
-            RETURN QUERY
-        SELECT MIN(date(transaction_datetime))
-		FROM transactions;
-        END IF;
-    END;
-	$$ LANGUAGE plpgsql;
-
-
-
-CREATE OR REPLACE FUNCTION test(ma date, mi date, average_factor NUMERIC, n1 NUMERIC, n2 NUMERIC, n3 NUMERIC)
-RETURNS TABLE (Customer_ID INTEGER, Required_Check_Measure NUMERIC, Group_Name VARCHAR, Offer_Discount_Depth NUMERIC)
+RETURNS SETOF date
 AS $$
 BEGIN
-        IF (ma > GetDates(1)) THEN
-            ma = GetDates(1);
-        ELSEIF (mi < GetDates(2)) THEN
-            mi = GetDates(2);
-        ELSEIF (mi >= ma) THEN
-            RAISE EXCEPTION
-                'Некорректный период';
-        END IF;
-		RETURN QUERY
-		WITH res1 AS (
-SELECT Personalinformation.customer_id AS c_id, (SUM(SKU_Summ)/COUNT(Personalinformation.customer_id)*average_factor)::NUMERIC AS Required
-FROM Personalinformation
-JOIN Cards ON Personalinformation.customer_id = Cards.customer_id
-JOIN Transactions ON Cards.customer_card_id = Transactions.customer_card_id
-JOIN Checks ON Checks.Transaction_id = Transactions.Transaction_id
-WHERE date(transaction_datetime) >= mi AND date(transaction_datetime)<= ma
-GROUP BY Personalinformation.customer_id)
-SELECT Customer, Required , G_Name , G_Discount NUMERIC 
-FROM res1
-JOIN nnn(n1, n2,n3) tmp ON res1.c_id = tmp.Customer;
+IF (key = 1) THEN
+RETURN QUERY
+SELECT MAX(date(transaction_datetime))
+FROM transactions;
+ELSEIF (key = 2) THEN
+RETURN QUERY
+SELECT MIN(date(transaction_datetime))
+FROM transactions;
+END IF;
 END;
 $$ LANGUAGE plpgsql;
 
 
 
-CREATE OR REPLACE FUNCTION nnn(n1 NUMERIC, n2 NUMERIC, n3 NUMERIC) RETURNS TABLE (Customer INTEGER, G_Name VARCHAR, G_Discount NUMERIC)
+CREATE OR REPLACE FUNCTION OffersGrowthCheck(first_date date, last_date date, average_factor NUMERIC, maximum_churn_index NUMERIC, maximum_share  NUMERIC, margin_share NUMERIC)
+RETURNS TABLE (Customer_ID INTEGER, Required_Check_Measure NUMERIC, Group_Name VARCHAR, Offer_Discount_Depth NUMERIC)
+AS $$
+BEGIN
+IF (last_date > GetDates(1)) THEN
+last_date = GetDates(1);
+ELSEIF (first_date < GetDates(2)) THEN
+first_date = GetDates(2);
+ELSEIF (first_date >= last_date) THEN
+RAISE EXCEPTION
+'Некорректный период';
+END IF;
+RETURN QUERY
+WITH res1 AS (
+SELECT Personalinformation.customer_id AS c_id, (SUM(SKU_Summ)/COUNT(Personalinformation.customer_id)*average_factor)::NUMERIC AS Required
+FROM Personalinformation
+JOIN Cards ON Personalinformation.customer_id = Cards.customer_id
+JOIN Transactions ON Cards.customer_card_id = Transactions.customer_card_id
+JOIN Checks ON Checks.Transaction_id = Transactions.Transaction_id
+WHERE date(transaction_datetime) >= first_date AND date(transaction_datetime)<= last_date
+GROUP BY Personalinformation.customer_id)
+SELECT Customer, ROUND(Required,2) , G_Name , G_Discount NUMERIC 
+FROM res1
+JOIN GetGroupNameAndDiscount(maximum_churn_index, maximum_share,margin_share) tmp ON res1.c_id = tmp.Customer;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION GetGroupNameAndDiscount(n1 NUMERIC, n2 NUMERIC, n3 NUMERIC) 
+RETURNS TABLE (Customer INTEGER, G_Name VARCHAR, G_Discount NUMERIC)
 AS $$
 BEGIN
 RETURN QUERY
@@ -84,4 +85,4 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-SELECT * FROM test('2023-08-20', '2015-01-20', 3, 10, 3,3)
+SELECT * FROM OffersGrowthCheck('2015-01-20', '2023-08-20', 3, 40 ,3, 3);
